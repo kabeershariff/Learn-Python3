9b5ca950253fbd4bb17e9f5f77e4098953382ce2
Omar Shariff <78196064+kabeershariff@users.noreply.github.com>
2023-05-22 00:01:02 +0530

    Add files via upload

---
+++ ex34.py
@@ -0,0 +1,10 @@
+#Accessing Elements of a List
+
+animals = ['bear', 'tiger', 'penguin', 'bull', 'horse']
+
+print("The List has ",animals)
+
+print("The item at index 0 ", animals[0])
+print("The item at index 3 ", animals[3])
+
+
