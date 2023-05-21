8d5c243d38f68ca055341ba354d84e90cf9f76bd
Omar Shariff <78196064+kabeershariff@users.noreply.github.com>
2023-05-21 20:08:13 +0530

    Add files via upload

---
+++ ex27.py
@@ -0,0 +1,28 @@
+#Logic in Python
+
+print("Lets see NOT.")
+print("not False = ", not False)
+print("not True = ", not True, "\n")
+
+print("Lets see OR Logic.")
+print("True or False = ", True or False)
+print("True or True = ", True or True)
+print("False or True = ", False or True)
+print("False or False = ", False or False, "\n")
+
+print("Lets see AND Logic.")
+print("True and False = ", True and False)
+print("True and True = ", True and True)
+print("False and True = ", False and True)
+print("False and False = ", False and False, "\n")
+
+print("Lets see NOT OR Logic.")
+print("not(True or False) = ", not(True or False))
+print("not(False or False) = ", not(False or False), "\n")
+
+print("Lets see NOT AND Logic.")
+print("not(True and False) = ", not(True and False))
+print("not(True and True) = ", not(True and True), "\n")
+
+print("Lets see (3 != 3) --> ", 3 != 3)
+print("Lets see (3 == 3) --> ", 3 == 3)
